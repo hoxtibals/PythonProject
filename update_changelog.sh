04bc2354fad34f712c8de007d901091ce2ff7c4c
hoxtibals <connor.hardee2002@gmail.com>
2023-11-06 12:00:16 -0500

    Create update_changelog.sh
    A script for us to automatically update change log

---
+++ update_changelog.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set the path to your CHANGELOG.md file
+changelog_file="CHANGELOG.md"
+
+# Fetch commit comments and append them to the changelog file
+git log --format="- %s" > "$changelog_file"
