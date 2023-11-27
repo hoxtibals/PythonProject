cf0531fdf8840366c1c6e0e60c068fc80d06fcd6
hoxtibals <connor.hardee2002@gmail.com>
2023-11-27 12:33:11 -0500

    Created button in view w/ basic load file command

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
 #It consists of pure application logic, which interacts with the database. 
 # It includes all the information to represent data to the end user.
+# this is where we would handle any WAV file manipulation
+
 
--- view.py
+++ view.py
@@ -3,6 +3,10 @@
 import tkinter as tk
 import matplotlib.pyplot as matplt
 
+def pickFile(path_file):
+    path_file.set(tk.filedialog.askopenfilename())
+    return path_file
+
 def StartWindow():
     mainWindow = tk.Tk()
     mainWindow.title("WAV file reader")
@@ -10,6 +14,14 @@ def StartWindow():
     #we need to use a figrure to display the graph inside Tkinter 
     graph = matplt.figure(figsize = (5,5), dpi = 100)
     plot = graph.add_subplot(1,1,1)
+    
+    #button to load WAV file we will need to import the command from another module
+    #we will use a string to keep track of file path, we will need to use
+    #LAMDA function to ensure the command is not called when the window is created
+    path_file = tk.StringVar()
+    load = tk.Button(mainWindow, text = "Load WAV file", command = lambda:pickFile(path_file))
+    load.pack()
+    #example input used to test plot
     plot.plot([1,2,3,4,5,6,7,8,9,10], [2,4,6,8,10,12,14,16,18,20])
     
     mainWindow.mainloop()
