fb69e76b766f4308499b055dd13f5ce271def6f3
hoxtibals <connor.hardee2002@gmail.com>
2023-12-10 21:53:12 -0500

    updated git log

--- update_changelog.sh
+++ update_changelog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the path to your CHANGELOG.md file
-changelog_file="CHANGELOG.md"
+changelog_file="copy_gitLog.md"
 
 # Fetch commit comments and append them to the changelog file
 git log --format="- %s (by %an at %ad)" >>"$changelog_file"
